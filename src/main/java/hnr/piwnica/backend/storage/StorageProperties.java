62a54af59c80870f5e35a90a6a1b7779ef0f98fb
Carbon225 <carbon225@protonmail.com>
2021-03-27 17:59:43 +0100

    use temp dir or env var for storage

--- src/main/java/hnr/piwnica/backend/storage/StorageProperties.java
+++ src/main/java/hnr/piwnica/backend/storage/StorageProperties.java
@@ -6,6 +6,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class StorageProperties {
     private String location = "imageDir";
 
+    public StorageProperties() {
+        var root = System.getenv("MAPTALK_STORAGE_DIR");
+        if (root == null || root.equals("")) {
+            root = System.getProperty("java.io.tmpdir") + "/maptalk-data";
+            System.out.println("Using temp dir");
+        }
+        location = root;
+    }
+
     public String getLocation() {
         return location;
     }
