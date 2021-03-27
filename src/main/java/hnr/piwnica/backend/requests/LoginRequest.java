f551cc7cb1292e6864034b70fd1eb20a817105b9
Carbon225 <carbon225@protonmail.com>
2021-03-27 15:23:29 +0100

    fix request models

--- src/main/java/hnr/piwnica/backend/requests/LoginRequest.java
+++ src/main/java/hnr/piwnica/backend/requests/LoginRequest.java
@@ -1,21 +1,9 @@
 package hnr.piwnica.backend.requests;
 
 public class LoginRequest {
-    private final String username;
-    private final String password;
-    private final Boolean rememberMe;
-
-    public LoginRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-        this.rememberMe = false;
-    }
-
-    public LoginRequest(String username, String password, Boolean rememberMe) {
-        this.username = username;
-        this.password = password;
-        this.rememberMe = rememberMe;
-    }
+    private String username;
+    private String password;
+    private Boolean rememberMe;
 
     public String getUsername() {
         return username;
--- src/main/java/hnr/piwnica/backend/requests/SignupRequest.java
+++ src/main/java/hnr/piwnica/backend/requests/SignupRequest.java
@@ -1,13 +1,8 @@
 package hnr.piwnica.backend.requests;
 
 public class SignupRequest {
-    private final String username;
-    private final String password;
-
-    public SignupRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
+    private String username;
+    private String password;
 
     public String getUsername() {
         return username;
